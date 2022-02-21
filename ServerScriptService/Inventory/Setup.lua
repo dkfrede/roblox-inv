d1e85fdd8f5833b2c44181f3ad58276085141a86
Fr3ckzDK <65610875+Fr3ckz@users.noreply.github.com>
2022-02-21 22:15:01 +0100

    Create Setup.lua

---
+++ ServerScriptService/Inventory/Setup.lua
@@ -0,0 +1,28 @@
+local Categori = Instance.new("Folder",game.ReplicatedStorage)
+Categori.Name = "Kates"
+
+for _, Categoris in pairs(game.ServerStorage.Inventory.Items:GetChildren()) do
+	print("hej1")
+	if Categoris:IsA("Folder") then
+		print("hej2")
+		if Categoris:FindFirstChild("Categori") then
+			print("hej")
+			local Kate = Instance.new("StringValue",game.ReplicatedStorage.Kates)
+			Kate.Name = Categoris.Name
+			Kate.Value = Categoris.Categori.Value
+		end
+	end
+end
+
+local Icons = Instance.new("Folder",game.ReplicatedStorage)
+Icons.Name = "Icons"
+
+for _, Icons in pairs(game.ServerStorage.Inventory.Items:GetChildren()) do
+	if Icons:IsA("Folder") then
+		if Icons:FindFirstChild("Icon") then
+			local Kate = Instance.new("Decal",game.ReplicatedStorage.Icons)
+			Kate.Name = Icons.Name
+			Kate.Texture = Icons.Icon.Texture
+		end
+	end
+end
