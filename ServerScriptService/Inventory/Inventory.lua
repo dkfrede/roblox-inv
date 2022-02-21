d03b86869c0fcfb953f57a78d5cc1ddd397b584b
Fr3ckzDK <65610875+Fr3ckz@users.noreply.github.com>
2022-02-21 22:14:44 +0100

    Create Inventory.lua

---
+++ ServerScriptService/Inventory/Inventory.lua
@@ -0,0 +1,43 @@
+local DataStoreService = game:GetService("DataStoreService")
+
+local InventoryData = DataStoreService:GetDataStore("SaveddInventoryd")
+
+local Module = require(script.Parent.Module)
+
+local function Added(player)
+	local Inventory = Instance.new("Folder",player)
+	Inventory.Name = "Inventory"
+	
+	-- DatastoreService
+	local data
+	local success,errormessage = pcall(function()
+		data = InventoryData:GetAsync("id-"..player.UserId)
+	end)
+	if success then
+		if data ~= nil then
+			for _, items in pairs(data) do
+				local inventory = Instance.new("IntValue",player:WaitForChild("Inventory"))
+				inventory.Name = items.Name
+				inventory.Value = items.Value
+			end
+		end
+	end
+	if Module.HaveInventoryItem(player,"Pensel",5) then
+		print("have items")
+	else
+		print("dont have item")
+	end
+end
+
+local function Removed(player)
+	local inventory = {}
+	if player:FindFirstChild("Inventory") then
+		for _, InventoryItems in pairs(player.Inventory:GetChildren()) do
+			table.insert(inventory,{Name = InventoryItems.Name,Value = InventoryItems.Value})
+		end
+	end
+	InventoryData:SetAsync("id-"..player.UserId,inventory)
+end
+
+game.Players.PlayerAdded:Connect(Added)
+game.Players.PlayerRemoving:Connect(Removed)
